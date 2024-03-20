ed6432f9c3d14419fd3e8c036520e1d274609b0f
LeVraiArdox <lvardox@proton.me>
2024-03-20 21:40:37 +0100

    Changed a few things in the skel

--- axskel/src/.local/share/plasma/look-and-feel/AxOS/contents/layouts/org.kde.plasma.desktop-layout.js
+++ axskel/src/.local/share/plasma/look-and-feel/AxOS/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -7,11 +7,11 @@ panelbottom.hiding = "none"
 /*spacer*/
 panelbottom.addWidget("org.kde.plasma.panelspacer")
 /*App Launcher*/
-menu = panelbottom.addWidget("AndromedaLauncher")
+menu = panelbottom.addWidget("Kickoff")
 menu.currentConfigGroup = ["General"]
 menu.writeConfig("activationIndicator", "false")
-menu.writeConfig("customButtonImage", "utilities-launcher")
-menu.writeConfig("floating", "true")
+menu.writeConfig("customButtonImage", "AxOS-logo")
+menu.writeConfig("floating", "false")
 menu.writeConfig("launcherPosition", "1")
 menu.writeConfig("useCustomButtonImage", "true")
 /*Desktops button*/
@@ -47,24 +47,6 @@ panelbottom_clock.writeConfig("fontSize", "11")
 panelbottom_clock.writeConfig("autoFontAndSize", "false")
 /*Notification*/
 panelbottom.addWidget("org.kde.plasma.notifications")
-
-/*Clock, Weather and Music Widget*/
-let desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-var desktopByClock = desktopsArray[j]
-}
-const NumX = Number(((screenGeometry(desktopByClock).width)-620)/2)
-const NumY = Number(((screenGeometry(desktopByClock).height)-240)/2)-panelbottom.height
-Clock = desktopByClock.addWidget("org.kde.plasma.Deskclock", NumX, NumY, 620, 240)
-
-if (knownWidgetTypes.includes('org.kde.plasma.weather')) {
-    const NumX2 = Number(((screenGeometry(desktopByClock).width)-256)/2)
-    const NumY2 = Number(((screenGeometry(desktopByClock).height)-160)/2)+120+panelbottom.height
-    weatherWidget = desktopByClock.addWidget("com.github.Widget.Weather.asymmetric.weather", NumX2, NumY2, 256, 160)
-    weatherWidget.currentConfigGroup = ["General"]
-    weatherWidget.writeConfig("source", "wettercom|weather|Mexico City, , MX|MX3530597;Mexico City")
-} else {
-   }
 
 /* accent color config*/
 ColorAccetFile = ConfigFile("kdeglobals")
