bc7fd5c40c2dabea2962894cac7773e564ce645a
LeVraiArdox <ardox@axos-project.com>
2024-07-29 13:52:14 +0200

    theme changes

--- axskel/src/plasma/look-and-feel/Deepin.Next.Dark.Plasma6/contents/layouts/org.kde.plasma.desktop-layout.js
+++ axskel/src/plasma/look-and-feel/Deepin.Next.Dark.Plasma6/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,7 +1,7 @@
 var panel = new Panel
 panel.location = "bottom"
 panel.hiding = "none"
-panel.height = 52
+panel.height = 51
 panel.alignment = "center"
 panel.floating = 1
 panel.lengthMode = "custom"
@@ -31,12 +31,16 @@ systray.currentConfigGroup = ["General"]
 let ListTrays = systray.readConfig("extraItems")
 systray.writeConfig("iconSpacing", 1)
 systray.writeConfig("hiddenItems", "Notificador de Discover_org.kde.DiscoverNotifier")
-panel.addWidget("Plasma.Control.Hub")
 
 panel_clock = panel.addWidget("org.kde.plasma.digitalclock")
 panel_clock.currentConfigGroup = ["Appearance"]
-panel_clock.writeConfig("fontSize", "14")
+panel_clock.writeConfig("fontSize", "12")
 panel_clock.writeConfig("autoFontAndSize", "false")
+panel_clock.writeConfig("showDate", "false");
+panel_clock.writeConfig("showDay", "false");
+panel_clock.writeConfig("displayEvents", "false");
+panel_clock.writeConfig("showSeconds", "false");
+panel_clock.writeConfig("timeFormat", "hh:mm");
 
 panel.addWidget("org.kde.plasma.trash")
 /*Buttons of aurorae*/
@@ -44,15 +48,6 @@ Buttons = ConfigFile("kwinrc")
 Buttons.group = "org.kde.kdecoration2"
 Buttons.writeEntry("ButtonsOnRight", "0")
 Buttons.writeEntry("ButtonsOnLeft", "XAI")
-
-/*Clock, Weather and Music Widget*/
-let desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-var desktopByClock = desktopsArray[j]
-}
-const NumX = Number(((screenGeometry(desktopByClock).width)-315)/2)
-const NumY = Number((screenGeometry(desktopByClock).height)*.17)
-desktopByClock.addWidget("CircleClock", NumX, NumY, 315, 315)
 
 /* accent color config*/
 ColorAccetFile = ConfigFile("kdeglobals")
