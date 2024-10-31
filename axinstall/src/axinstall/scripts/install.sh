848f3e5454b3a991f511725fb1c5a1a62bee7d39
Ardox <ardox@axos-project.com>
2024-10-31 15:06:16 +0100

    Axinstall GUI: keyring init before starting installation

--- axinstall/src/axinstall/scripts/install.sh
+++ axinstall/src/axinstall/scripts/install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 echo "Running reflector to sort for fastest mirrors" | tee -a /tmp/axinstall-output.txt
 pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist | tee -a /tmp/axinstall-output.txt
+echo "Initializing pacman keyring" | tee -a /tmp/axinstall-output.txt
+pkexec pacman-key --init | tee -a /tmp/axinstall-output.txt
+pkexec pacman-key --populate archlinux | tee -a /tmp/axinstall-output.txt
+echo "Starting installation" | tee -a /tmp/axinstall-output.txt
 pkexec axinstall-cli config ~/.config/axinstall.json | tee -a /tmp/axinstall-output.txt
